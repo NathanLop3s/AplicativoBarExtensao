1578eecf39b22869234c13c373f68b9abf884af7
Nathan Lopes <nathan.lopes@gmail.com>
2026-04-10 14:42:06 -0300

    pequena mudança no routes

--- backend/src/routes/usuario.routes.ts
+++ backend/src/routes/usuario.routes.ts
@@ -15,7 +15,7 @@ router.post('/login', async (req, res) => {
         );
 
         if (rows.length === 0) {
-            return res.status(401).json({ erro: 'Usuário não encontrado' });
+            return res.status(401).json({ erro: 'Credenciais inválidas' });
         }
 
         const usuario = rows[0];
