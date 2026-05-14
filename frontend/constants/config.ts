e9c2383df732bd6de3751adc4b2e7e1804cbe75f
Nathan Lopes <nathan.lopes@gmail.com>
2026-05-14 14:28:42 -0300

    fixes

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -1,4 +1,4 @@
-//export const API_URL = 'https://aplicativobarextensao.onrender.com';
-export const API_URL = 'http://192.168.18.100:8080';
+export const API_URL = 'https://aplicativobarextensao.onrender.com';
+//export const API_URL = 'http://192.168.18.100:8080';
 
 
